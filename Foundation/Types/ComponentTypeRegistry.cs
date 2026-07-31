035f308f454b5b2ff109d24a05a976104d880f81
TIGames <kisstp2006@gmail.com>
2026-07-31 17:14:37 +0200

    Add ComponentTypeRegistry for type-index mapping
    Introduced ComponentTypeRegistry in Foundation.Types to manage unique integer indices for component types using a Dictionary<Type, int>. Provides a generic GetIndex<T>() method for retrieving or assigning indices.

---
+++ Foundation/Types/ComponentTypeRegistry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foundation.Types
+{
+    public class ComponentTypeRegistry
+    {
+        private Dictionary<Type, int> typeToIndex = new Dictionary<Type, int>();
+        
+        private int nextIndex = 0;
+
+        public int GetIndex<T>()
+        {
+            if (typeToIndex.TryGetValue(typeof(T), out int index)) // We check if theres a component type in the list we use its index if not we get a new one
+            {
+                return index;
+            }
+
+            int newIndex = nextIndex;
+            typeToIndex[typeof(T)] = newIndex;
+            nextIndex++;
+
+            return newIndex;
+        }
+
+    }
+}
