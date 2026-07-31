66d6f74d62c28d53f8afca42daf4deea8084c901
TIGames <kisstp2006@gmail.com>
2026-07-31 12:18:09 +0200

    Add Color struct to Foundation.Math with RGBA support
    Introduced a Color struct representing RGBA colors with float components. Includes constructors, Set and ToArray methods, and predefined static readonly colors such as Red, Yellow, Green, Blue, Black, and White.

---
+++ Foundation/Math/Color.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Foundation.Math
+{
+    public struct Color
+    {
+        public float r, g, b, a;
+
+        public Color()
+        {
+            r = 0.0f;
+            g = 0.0f;
+            b = 0.0f;
+            a = 1.0f;
+        }
+
+        public Color(float r, float g, float b, float a = 1.0f)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = a;
+        }
+
+        public void Set(float r, float g, float b, float a = 1.0f)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = a;
+        }
+
+        public float[] ToArray()
+        {
+            return new float[4] { r, g, b, a };
+        }
+
+        public static readonly Color Red = new Color(1.0f, 0.0f, 0.0f);
+        public static readonly Color Yellow = new Color(1.0f, 1.0f, 0.0f);
+        public static readonly Color Green = new Color(0.0f, 1.0f, 0.0f);
+        public static readonly Color Blue = new Color(0.0f, 0.0f, 1.0f);
+        public static readonly Color Black = new Color(0.0f, 0.0f, 0.0f);
+        public static readonly Color White = new Color(1.0f, 1.0f, 1.0f);
+
+    }
+}
