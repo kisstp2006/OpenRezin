c24f69040d3e573f37c8a6fec77a32e4c708b8c1
TIGames <kisstp2006@gmail.com>
2026-07-31 16:55:53 +0200

    Add type-safe event system with dispatcher and subscription
    Introduced EventDispatcher and EventSubscription<TEvent> in Foundation.Communication. Enables type-safe event subscription, dispatch, and automatic unsubscription via IDisposable. Uses delegates for flexible event handling.

---
+++ Foundation/Communication/EventDispatcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foundation.Communication
+{
+    public class EventDispatcher 
+    {
+        private Dictionary<Type, List<Delegate>> handlers = new Dictionary<Type, List<Delegate>>();
+
+
+
+        public IDisposable Subscribe<TEvent>(Action<TEvent> handler)
+        {
+            if (!handlers.TryGetValue(typeof(TEvent), out List<Delegate>? list))
+            {
+                list = new List<Delegate>();
+                handlers[typeof(TEvent)] = list;
+            }
+            list.Add(handler);
+
+            return new EventSubscription<TEvent>(this, handler);
+        }
+        public void Dispatch<TEvent>(TEvent evt)
+        {
+            if (!handlers.TryGetValue(typeof(TEvent), out List<Delegate>? list))
+                return;
+            if (!(list.Count > 0)) return;
+
+            foreach (Delegate handler in list)
+            {
+                ((Action<TEvent>)handler)(evt);
+            }
+
+        }
+        public bool HasReceivers<TEvent>()
+        {
+            if (handlers.TryGetValue(typeof(TEvent), out List<Delegate>? list))
+            {
+                return list.Count > 0;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Unsubscribe<TEvent>(Action<TEvent> handler)
+        {
+            if (handlers.TryGetValue(typeof(TEvent), out List<Delegate>? list))
+            {
+                list.Remove(handler);
+            }
+        }
+    }
+}
---
+++ Foundation/Communication/EventSubscription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foundation.Communication
+{
+    public class EventSubscription<TEvent> : IDisposable
+    {
+        private EventDispatcher dispatcher;
+        private Action<TEvent> handler;
+
+        public EventSubscription(EventDispatcher dispatcher, Action<TEvent> handler)
+        {
+            this.handler = handler;
+            this.dispatcher = dispatcher;
+
+        }
+
+        public void Dispose()
+        {
+            dispatcher.Unsubscribe(handler);
+        }
+    }
+}
