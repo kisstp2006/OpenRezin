575c235fec0c0ef08ca6c9fbeee890d8385a7729
TIGames <kisstp2006@gmail.com>
2026-07-31 11:13:11 +0200

    Add FileWriter class with logging and IDisposable support
    Introduced the FileWriter class in Foundation.IO to handle file operations with open, write, and close methods. Implements IDisposable for resource management and integrates Logger for error handling during file operations.

---
+++ Foundation/IO/FileWriter.cs
@@ -0,0 +1,63 @@
+﻿using Foundation.Logger;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foundation.IO 
+{
+    public class FileWriter : IDisposable
+    {
+        private FileStream? stream;
+
+        public bool Open(string path)
+        {
+            try
+            {
+                stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                return true;
+
+            }
+            catch (IOException e)
+            {
+                Log.Error("Failed to open file: " + e.Message);
+
+                return false;
+            }
+        }
+
+        public void Close()
+        {
+            if (stream == null) return;
+            try
+            {
+                stream.Close();
+            }
+            catch (IOException e)
+            {
+                Log.Error("Failed to close file: " + e.Message);
+
+            }
+
+        }
+
+        public void WriteBytes(byte[] bytes, int offset, int count)
+        {
+            if (stream == null) return;
+            try
+            {
+                stream.Write(bytes, offset, count);
+            }
+            catch (IOException e)
+            {
+                Log.Error("Failed to write bytes: " + e.Message);
+                return;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Close();
+        }
+    }
+}
