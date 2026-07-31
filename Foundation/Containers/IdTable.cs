d9e19eac181a19a797ead83fe5304fe04b7de6d2
TIGames <kisstp2006@gmail.com>
2026-07-31 14:17:55 +0200

    Add IdTable<T> with generational Id support
    Introduced IdTable<T> in Foundation.Containers for safe value management using generational indices. Added Id struct in Foundation.Types as a handle with equality, hashing, and validity checks to prevent stale access.

---
+++ Foundation/Containers/IdTable.cs
@@ -0,0 +1,68 @@
+﻿
+using Foundation.Types;
+
+namespace Foundation.Containers
+{
+    public class IdTable<T>
+    {
+        private List<T> values = new List<T>();
+        private List<int> generations = new List<int>();
+        private Stack<int> freeIndices = new Stack<int>();
+        public Id Insert(T value) 
+        {
+            if (freeIndices.Count > 0) 
+            {
+                int index = freeIndices.Pop(); // We search the first free slot
+
+                values[index] = value; // we give the new value to the new free slot
+
+                return new Id(index, generations[index]); // then we return a new Id
+            }
+            else
+            {
+                values.Add(value);
+                generations.Add(0);
+                int index = values.Count - 1;
+                return new Id(index, generations[index]);
+            }
+
+        }
+
+        public void Remove(Id id)
+        {
+            if (IsValidId(id))
+            {
+                if (id.Generation == generations[id.Index])
+                {
+                    generations[id.Index]++;
+
+                    freeIndices.Push(id.Index);
+
+                    values[id.Index] = default;
+                }
+            }
+
+
+
+        }
+        private bool IsValidId(Id id)
+        {
+            if (!(id.Index >= 0)) return false;
+            if (!id.IsValid) return false;
+            if (!(id.Index < values.Count)) return false;
+
+            return id.Generation == generations[id.Index];
+        }
+        public bool TryGet(Id id, out T value)
+        {
+            if (IsValidId(id))
+            {
+                value = values[id.Index];
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+    }
+}
---
+++ Foundation/Types/Id.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foundation.Types
+{
+    public struct Id : IEquatable<Id>
+    {
+        public int Index;
+        public int Generation;
+
+        public Id(int index, int generation)
+        {
+            Index = index;
+            Generation = generation;
+        }
+
+        public static readonly Id Invalid = new Id(-1, 0);
+
+        public bool IsValid => Index != -1;
+
+        public  bool Equals(Id other) => Index == other.Index && Generation == other.Generation;
+
+        public override bool Equals(object? obj) => obj is Id other && Equals(other);
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Index, Generation);
+        }
+        public static bool operator ==(Id left, Id right) => left.Equals(right);
+        public static bool operator !=(Id left, Id right) => !left.Equals(right);
+
+    }
+}
